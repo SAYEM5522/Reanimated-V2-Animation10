094e59af55536b644256d8be98da81aec0b7e28b
SAYEM5522 <md1040582@gmail.com>
2021-10-26 12:04:29 +0600

    Details add

--- App.js
+++ App.js
@@ -1,18 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import SearchBar from './Component/SearchBar';
+import { AntDesign } from '@expo/vector-icons';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eff5f2',
   
   },
+  BackIcon:{
+    backgroundColor:'#fff',
+    borderRadius:10,
+    width:50,
+    height:45,
+    left:20,
+    top:25,
+    padding:10
+  },
+  Tittle:{
+    fontSize:30,
+    width:150,
+    height:100,
+    fontWeight:'bold',
+    top:40,
+    left:30
+  },
+  Image:{
+    height:350,
+    width:200
+  },
+  Details:{
+    flexDirection:'row',
+    alignItems:"flex-start"
+  }
 });
 export default function App() {
   return (
     <View style={styles.container}>
       <SearchBar/>
+      <AntDesign name="left" style={styles.BackIcon} size={24} color="black" />
+      <View style={styles.Details}>
+        <Text style={styles.Tittle}>Choose Your CV</Text>
+        <Image
+        source={{uri:'https://o.remove.bg/downloads/49b8ef79-a540-4b65-abe5-45864cecf72b/istockphoto-1200677760-170667a-removebg-preview.png'}}
+        style={styles.Image}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
---
+++ Component/Data.js
@@ -0,0 +1,29 @@
+export const data=[
+
+  {
+    id:"1",
+    img:"https://i2-prod.mirror.co.uk/incoming/article22028564.ece/ALTERNATES/s1200c/0_hp-teaser-sg-m-desktop.jpg",
+    name:"Product Deginer",
+    price:"$900"
+  },
+  {
+    id:"2",
+    img:"https://media.istockphoto.com/photos/portrait-of-an-african-american-young-woman-in-sunglasses-isolated-on-picture-id1201881974?k=20&m=1201881974&s=612x612&w=0&h=X41gI8ph_9i3CjyPeorHj9U95g8aeNQPX79_CEpx4nc=",
+    name:"UI/UX Deginer",
+    price:"$1000"
+  },
+  {
+    id:"3",
+    img:"https://c2.peakpx.com/wallpaper/107/26/326/2k-dark-guy-male-man-wallpaper.jpg",
+    name:"Visual Deginer",
+    price:"$1000"
+  },
+  
+  {
+    id:"4",
+    img:"https://images.unsplash.com/photo-1501441858156-e505fb04bfbc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8c3VuZ2xhc3NlcyUyMHdvbWFufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    name:"UX Deginer",
+    price:"$1200"
+  },
+ 
+  ]
