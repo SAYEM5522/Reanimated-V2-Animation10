a522e06cfcb22f8202d0535737b3a3797ab1ebee
SAYEM5522 <md1040582@gmail.com>
2021-10-26 10:54:24 +0600

    SearchBar Add

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import SearchBar from './Component/SearchBar';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  
+  },
+});
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start</Text>
+      <SearchBar/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ Component/SearchBar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { FontAwesome5 } from '@expo/vector-icons';
+const SearchBar = () => {
+  return (
+    <View style={styles.Container}>
+    <FontAwesome5 name="search" style={styles.Icon} size={24} color="white" />
+    </View>
+  )
+}
+
+export default SearchBar
+
+const styles = StyleSheet.create({
+  Container:{
+    position:'absolute',
+    bottom:20,
+    alignSelf:'center',
+    width:95,
+    height:80,
+    borderRadius:20,
+    backgroundColor:'black',
+  },
+  Icon:{
+   left:35,
+   top:25
+    
+  }
+})
