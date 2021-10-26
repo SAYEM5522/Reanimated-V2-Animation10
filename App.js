42e35966661c9d91828ddf41cea7957240990c98
SAYEM5522 <md1040582@gmail.com>
2021-10-26 19:06:24 +0600

    Graph Add

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
 import SearchBar from './Component/SearchBar';
 import { AntDesign } from '@expo/vector-icons';
+const {width: SIZE} = Dimensions.get('window');
+import {ChartDot, ChartPath, ChartPathProvider, monotoneCubicInterpolation} from '@rainbow-me/animated-charts';
+const data = [
+  {x: 1453075200, y: 1.47}, {x: 1453161600, y: 1.37},
+  {x: 1453248000, y: 1.53}, {x: 1453334400, y: 1.54},
+  {x: 1453420800, y: 1.52}, {x: 1453507200, y: 2.03},
+  {x: 1453593600, y: 2.10}, {x: 1453680000, y: 2.50},
+  {x: 1453766400, y: 2.30}, {x: 1453852800, y: 2.42},
+  {x: 1453939200, y: 2.55}, {x: 1454025600, y: 2.41},
+  {x: 1454112000, y: 2.43}, {x: 1454198400, y: 2.20},
+];
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -28,14 +39,20 @@ const styles = StyleSheet.create({
   },
   Image:{
     height:350,
-    width:200
+    width:200,
+    left:20
   },
   Details:{
     flexDirection:'row',
     alignItems:"flex-start"
+  },
+  Graph:{
+    position:'absolute',
+    top:SIZE/2
   }
 });
 export default function App() {
+  const points = monotoneCubicInterpolation({data, range: 40});
   return (
     <View style={styles.container}>
       <SearchBar/>
@@ -47,6 +64,12 @@ export default function App() {
         style={styles.Image}
         />
       </View>
+      <View style={styles.Graph}>
+    <ChartPathProvider data={{ points, smoothingStrategy: 'bezier' }}>
+      <ChartPath strokeWidth={4} selectedStrokeWidth={4} height={SIZE / 2} selectedOpacity={1} stroke="#dbb144" width={SIZE-100} />
+      <ChartDot size={14} style={{ backgroundColor: 'blue',}} />
+    </ChartPathProvider>
+     </View>
       <StatusBar style="auto" />
     </View>
   );
